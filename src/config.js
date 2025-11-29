d695969232794fda57563664107d62a474b2cefa
bish9oi <bishnoiravindra344@gmail.com>
2025-11-29 22:36:37 +0530

    config

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    supabaseUrl: "https://pwjjxykvquevlnnytoif.supabase.co",
-    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3amp4eWt2cXVldmxubnl0b2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MzI2MjYsImV4cCI6MjA3MjEwODYyNn0.84s468HozBePkbsxFuGJfPKV3ZlE4efSTmh0mJmk4MA",
-    supabaseServiceRole: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3amp4eWt2cXVldmxubnl0b2lmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjUzMjYyNiwiZXhwIjoyMDcyMTA4NjI2fQ.HZlVe4X5A61go9DRAjH7o01QDwq2JgP_MZCoChJseZM",
-    pythonApi: import.meta.env.VITE_PYTHON_API,
-    javaApi: import.meta.env.VITE_JAVA_API,
-}
-
-export default config;
